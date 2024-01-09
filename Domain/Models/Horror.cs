6ad1d60b599d065bc7ff6bcfd7ef8fa2339f7465
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2024-01-09 09:34:14 +0100

    Created Horror model

---
+++ Domain/Models/Horror.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class Horror
+    {
+        public int ScaryLevel { get; set; }
+        public bool SupernaturalElements { get; set; }
+        public int GoreLevel { get; set; }
+    }
+}
