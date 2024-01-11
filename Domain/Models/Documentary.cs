ddf31b4e759f6e6af68fb28f0f1443b00c422f1e
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2024-01-11 11:51:37 +0100

    Created Documentary Model

---
+++ Domain/Models/Documentary.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models
+{
+    public class Documentary : Movie
+    {
+        public required string HistoricalContext { get; set; }
+        public required string RealLifeContext { get; set; }
+    }
+}
