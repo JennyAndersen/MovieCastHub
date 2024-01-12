853c65bf3dc9e18566e1e8f34e800362a1a37075
MP4940 <maxpersson40@gmail.com>
2024-01-12 10:37:27 +0100

    Created User model and added properties

---
+++ Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        //FavoriteGenre
+    }
+}
