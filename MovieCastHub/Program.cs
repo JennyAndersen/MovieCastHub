694b8a202091d75e00e6fbf26ba24aec342bf4a2
lokakos01 <114986974+lokakos01@users.noreply.github.com>
2024-01-15 11:11:45 +0100

    Fixed White Spaces

--- MovieCastHub/Program.cs
+++ MovieCastHub/Program.cs
@@ -13,7 +13,7 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-  
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
