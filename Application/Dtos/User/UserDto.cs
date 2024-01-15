29da9370ce1656337f8642e8742d763bd6baf9e8
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2024-01-15 12:12:13 +0100

    jag har lagt till userdto samt userupdate dto jag har även lagt till interface för repon

---
+++ Application/Dtos/User/UserDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Application.Dtos.User
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository
+    {
+
+    }
+}
