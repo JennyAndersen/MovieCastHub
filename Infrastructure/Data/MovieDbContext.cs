2e77756317830931fd64df636105a894f30f5639
lokakos01 <114986974+lokakos01@users.noreply.github.com>
2024-01-15 10:27:24 +0100

    Created MovieDbContext plus injected coonectionstring

---
+++ Infrastructure/Data/MovieDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Comedy> ComedyMovies { get; set; }
+        public virtual DbSet<Horror> HorrorMovies { get; set; }
+        public virtual DbSet<Documentary> DocumentaryMovies { get; set;}
+        public virtual DbSet<UserMovie> UserMovie {  get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS; Database=MovieCastHubDb; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+    }
+}
