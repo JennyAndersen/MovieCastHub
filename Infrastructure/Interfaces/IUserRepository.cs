626f90ba3c7677fe82de3193a3315c94d207e37d
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2024-01-15 13:12:14 +0100

    merge conflicts resolved

---
+++ Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(Guid userId);
+        Task<User> CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(Guid userId);
+    }
+}
