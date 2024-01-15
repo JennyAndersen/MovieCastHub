1381f721a19c29e8dd86a3276e41abf6d6249032
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2024-01-15 14:00:32 +0100

    I have Added User repository and interface for repo, i also added Dtos for user and i did scope in dependency injection

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -1,9 +1,50 @@
-﻿
+﻿#nullable disable
+using Domain.Models;
+using Infrastructure.Data;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly MovieDbContext _context;
 
+        public UserRepository(MovieDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task DeleteUserAsync(Guid userId)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(Guid userId)
+        {
+            return await _context.Users.FindAsync(userId);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
     }
 }
