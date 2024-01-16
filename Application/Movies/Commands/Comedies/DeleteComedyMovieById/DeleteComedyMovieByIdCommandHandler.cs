071f844297740e0f0d49e6c037f6c01580093364
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2024-01-16 15:35:42 +0100

    Created endpoint for Deleting Comedy Movie

---
+++ Application/Movies/Commands/Comedies/DeleteComedyMovieById/DeleteComedyMovieByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Movies.Commands.Comedies.DeleteComedyMovieById
+{
+    public class DeleteComedyMovieByIdCommandHandler : IRequestHandler<DeleteComedyMovieByIdCommand, bool>
+    {
+        private readonly IMovieRepository _movieRepository;
+        public DeleteComedyMovieByIdCommandHandler(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public async Task<bool> Handle(DeleteComedyMovieByIdCommand request, CancellationToken cancellationToken)
+        {
+            Movie comedyMovieToDelete = await _movieRepository.GetByIdAsync(request.MovieId);
+
+            if (comedyMovieToDelete == null)
+            {
+                return false;
+            }
+
+            await _movieRepository.DeleteAsync(request.MovieId);
+            return true;
+        }
+    }
+}
