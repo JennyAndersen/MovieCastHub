eb154dc9fa472b018d27a0db281157c185243dd9
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2024-01-17 11:59:24 +0100

    Refactorization for test is done, we also added CustomAutoDataAttribute for our tests

---
+++ Test/Application/UserTest/QueryHandlers/GetAllUsersQueryHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.Users.Commands.DeleteUser;
+using Application.Users.Querys.GetAllUsers;
+using Domain.Models;
+using FluentAssertions;
+using Infrastructure.Interfaces;
+using Moq;
+using NUnit.Framework;
+using Test.TestHelpers;
+
+
+namespace Test.Application.UserUnitTest.QueryTests
+{
+    [TestFixture]
+    public class GetAllUsersQueryHandlerTest
+    {
+        private Mock<IUserRepository> _mockUserRepository;
+        private GetAllUsersQueryHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _handler = new GetAllUsersQueryHandler(_mockUserRepository.Object);
+        }
+
+        [Test, CustomAutoData]
+        public async Task GetAllUsers_ShouldReturnAllUsers(List<User> users)
+        {
+            //Arrange
+            _mockUserRepository.Setup(repo => repo.GetAllUsersAsync()).ReturnsAsync(users);
+            var query = new GetAllUsersQuery();
+
+            //Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            result.Should().BeEquivalentTo(users);
+        }
+    }
+}
