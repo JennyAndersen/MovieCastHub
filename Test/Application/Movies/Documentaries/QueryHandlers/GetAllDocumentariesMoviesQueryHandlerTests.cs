ee9d00a0be13f045bfd4c4117443f053339ca036
MP4940 <maxpersson40@gmail.com>
2024-01-23 14:07:17 +0100

    Added unit tests for documentary queryHandlers and updated getAllQueryHandler for documentary to fit unit tests

---
+++ Test/Application/Movies/Documentaries/QueryHandlers/GetAllDocumentariesMoviesQueryHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Movies.Queries.Documentaries.GetAllDocumentary;
+using AutoFixture.NUnit3;
+using Domain.Models;
+using Infrastructure.Interfaces;
+using Moq;
+using NUnit.Framework;
+using Test.TestHelpers;
+
+namespace Test.Application.Movies.Documentaries.QueryHandlers
+{
+    [TestFixture]
+    public class GetAllDocumentariesMoviesQueryHandlerTests
+    {
+        private Mock<IMovieRepository> _movieRepositoryMock;
+        private GetAllDocumentaryMoviesQueryHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _movieRepositoryMock = new Mock<IMovieRepository>();
+            _handler = new GetAllDocumentaryMoviesQueryHandler(_movieRepositoryMock.Object);
+        }
+
+        [Test]
+        [CustomAutoData]
+        public async Task WHEN_Handle_THEN_GetAlLBirds_ReturnsCorrect([Frozen] List<Documentary> documentaries)
+        {
+            // Arrange
+            _movieRepositoryMock.Setup(x => x.GetAllDocumentaryMoviesAsync()).ReturnsAsync(documentaries);
+
+            var query = new GetAllDocumentaryMoviesQuery();
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<List<Documentary>>());
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
+}
---
+++ Test/Application/Movies/Documentaries/QueryHandlers/GetDocumentaryMovieByTitleQueryHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.Movies.Queries.Documentaries.GetDocumentaryMovieByTitle;
+using AutoFixture.NUnit3;
+using Domain.Models;
+using Infrastructure.Interfaces;
+using Moq;
+using NUnit.Framework;
+using Test.TestHelpers;
+
+namespace Test.Application.Movies.Documentaries.QueryHandlers
+{
+    public class GetDocumentaryMovieByTitleQueryHandlerTests
+    {
+        private GetDocumentaryMovieByTitleQueryHandler _handler;
+        private Mock<IMovieRepository> _movieRepositoryMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _movieRepositoryMock = new Mock<IMovieRepository>();
+            _handler = new GetDocumentaryMovieByTitleQueryHandler(_movieRepositoryMock.Object);
+        }
+
+        [Test]
+        [CustomAutoData]
+        public async Task WHEN_Handle_THEN_ValidTitle_ReturnsCorrectMovies([Frozen] Movie documentaryMovie, List<Movie> documentaryMovies)
+        {
+            // Arrange
+            _movieRepositoryMock.Setup(x => x.GetByTitleAsync(documentaryMovie.Title)).ReturnsAsync(documentaryMovies);
+            var query = new GetDocumentaryMovieByTitleQuery(documentaryMovie.Title);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
